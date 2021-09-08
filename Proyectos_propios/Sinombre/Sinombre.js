6c4d0fd490c35b83dee7cc127442832a246b626d
Crokidokis <cocarochagonzalo@gmail.com>
2021-09-08 03:19:22 -0300

    C

---
+++ Proyectos_propios/Sinombre/Sinombre.js
@@ -0,0 +1,5 @@
+var jsonobject = '{"Marca":"Bajaj","Modelo": "Ns 200 Fi","Cilindrada": "200 cc","Tipo": "Monocilindrico","Caballaje": "24.8 cv","Torque": "18.6 Nm","Peso en orden de marcha": "155 kg","Freno delantero": "Disco abs","Freno trasero": "Disco abs","Alimentacion": "Inyeccion electronica"}'
+var motos = JSON.parse(jsonobject);
+console.log (motos);
+
+
