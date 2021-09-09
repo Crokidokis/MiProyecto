a05efb50445e592d178ffcf310456507049cb2e5
Crokidokis <cocarochagonzalo@gmail.com>
2021-09-09 15:12:31 -0300

    Importjsonobject

--- Proyectos_propios/Sinombre/Sinombre.js
+++ Proyectos_propios/Sinombre/Sinombre.js
@@ -1,5 +1,4 @@
-var jsonobject = '{"Marca":"Bajaj","Modelo": "Ns 200 Fi","Cilindrada": "200 cc","Tipo": "Monocilindrico","Caballaje": "24.8 cv","Torque": "18.6 Nm","Peso en orden de marcha": "155 kg","Freno delantero": "Disco abs","Freno trasero": "Disco abs","Alimentacion": "Inyeccion electronica"}'
-var motos = JSON.parse(jsonobject);
-console.log(motos);
+var jsonobject = require ('./Sinombre.json')
+jsonobject.Motos200_250cc
 
 
